17c49833a0d9c70627881e0ca6b89b1129fcf2ec
aGhouzlane <ahmedghouzlane@gmail.com>
2021-06-15 20:37:15 -0400

    Project rebuild

---
+++ SweetTreats/Models/SweetTreatsContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetTreats.Models
+{
+  public class SweetTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+    public SweetTreatsContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
---
+++ SweetTreats/Models/Treat.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SweetTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; }
+  }
+}
